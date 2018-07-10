dd2ff91e79f0706c8448e865ae3e545753b4487b
Brion <brionqiye@gmail.com>
2018-07-09 21:30:16 -0700

    Working code for printing a gradient on screen in the form of a QLabel of QPixmap of QImage

--- Application.py
+++ Application.py
@@ -7,6 +7,7 @@ from Data import AnalogData as input
 defw = 1000
 defh = 600
 
+
 class Application(QWidget):
     def __init__(self):
         super().__init__()
@@ -15,17 +16,35 @@ class Application(QWidget):
     r = 0
     g = 0
     b = 0
-    def initUI(self):
-        scanA = QImage(500, 500, 24)
-        scanPaint = QPainter(scanA)
-        scanPixmap = QGraphicsPixmapItem()
-        scanPixmap.setPixmap()
-        scanScene = QGraphicsScene()
-        scanScene.addPixmap(scanPixmap)
-        scanView = QGraphicsView(scanScene)
-        scanView.show()
+    scanA = QImage('Yellow_BG.JPG')
 
-        scanPaint.drawRect(50, 50, 50, 50)
+    def showImage(self):
+        return
+
+    def drawImage(self):
+        p = QPainter(self.scanA)
+        for i in range(500):
+            p.setPen(QColor(i / 2, i / 2, i / 2, 255))
+            p.drawLine(i, 0, i, 500)
+        p.drawRect(0, 0, 500, 500)
+        return
+    def initUI(self):
+        self.drawImage()
+        scanPixmap = QPixmap()
+        scanPixmap.convertFromImage(self.scanA)
+        scanLabel = QLabel('Scan Area', self)
+        scanLabel.setPixmap(scanPixmap)
+        scanLabel.resize(scanLabel.sizeHint())
+        scanLabel.move(50, 50)
+        scanLabel.show()
+        # scanLabel.deleteLater()
+        # self.scanPaint.setPen(QColor(50, 50, 50, 50))
+        # self.scanPaint.drawRect(50, 50, 50, 50)
+        # scanPixmap.convertFromImage(self.scanA)
+        # scanLabel2 = QLabel('Scan Area', self)
+        # scanLabel2.setPixmap(scanPixmap)
+        # scanLabel2.move(100, 100)
+        # scanLabel2.show()
 
         quitb = QPushButton('Quit', self)
         quitb.clicked.connect(QApplication.instance().quit)
@@ -33,11 +52,10 @@ class Application(QWidget):
         quitb.move(defw-150, 60)
 
         scanb = QPushButton('Start Scan', self)
-        scanb.clicked.connect(scanArea.paintit)
+        # scanb.clicked.connect(self.drawImage())
 
         scanb.resize(scanb.sizeHint())
         scanb.move(defw - 150, 120)
-        self.paintit()
         print("Ready to go!")
 
         self.setFixedWidth(defw)
--- Testing.py
+++ Testing.py
@@ -1,57 +1,42 @@
 import sys
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import *
 
-defw = 1000
-defh = 600
 
+class App(QWidget):
 
-class Application(QWidget):
     def __init__(self):
         super().__init__()
+        self.title = 'PyQt5 image'
+        self.left = 100
+        self.top = 100
+        self.width = 640
+        self.height = 480
         self.initUI()
 
-    r = 50
-    g = 50
-    b = 50
-
     def initUI(self):
-        quitb = QPushButton('Redraw', self)
-        quitb.clicked.connect(Application.paintit)
-        quitb.resize(quitb.sizeHint())
-        quitb.move(defw - 150, 60)
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.setFixedWidth(defw)
-        self.setFixedHeight(defh)
-        self.setWindowTitle('Refresh Problem')
+        # Create widget
+        label = QLabel(self)
+        image = QImage(100, 100, 24)
+        image.load('Yellow_BG.JPG')
+
+        paint = QPainter()
+        paint.begin(image)
+        paint.drawRect(20, 20, 20, 20)
+        paint.end()
+        # pixmap = QPixmap('Cool Logo.jpg')
+        pixmap = QPixmap()
+        pixmap.convertFromImage(image)
+        label.setPixmap(pixmap)
+        self.resize(pixmap.width(), pixmap.height())
+
         self.show()
-
-    def paintit(self):
-        self.update()
-
-    def paintEvent(self, event):
-        scanarea = QStylePainter()
-        scanarea.begin(self)
-        print("yo I'm painting")
-        print(self.r)
-        print(self.g)
-        print(self.b)
-        scanarea.drawPoint(self.r + 50, self.g + 50)
-
-        # for i in range(50, 550):
-        #     for j in range(50, 550):
-        #         scanarea.drawPoint(i, j)
-        # scanarea.setPen(QColor(self.r, self.g, self.b, 0))
-        self.r += 1
-        if self.r > 250: self.r = 0
-        self.g += 1
-        if self.g > 250: self.g = 0
-        self.b += 1
-        if self.b > 250: self.b = 0
-        scanarea.end()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Application()
+    ex = App()
     sys.exit(app.exec_())
