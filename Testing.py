613be057915be6b3a11e574ea44341d2e7e9e624
BYTurnips <brionqiye@gmail.com>
2018-07-18 23:09:20 -0700

    More misc progress on the application. I drafted a procedure for the signal slot thing

--- Testing.py
+++ Testing.py
@@ -6,57 +6,69 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-defw = 1000
-defh = 600
 
-
-class compute(QThread):
-
-    def __init__(self, scanA):
-        QThread.__init__(self)
-        self.r = 0
-        self.g = 0
-        self.b = 0
-        self.scanA = scanA
+class data(QThread):
+    def __init__(self):
+        super().__init__()
 
     def run(self):
-        p = QPainter()
-        p.begin(self.scanA)
-        # p.fillRect(0, 0, 500, 500, QColor(self.r, self.g, self.b, 250))
-        p.setPen(QColor(self.r, self.g, self.b, 250))
-        for i in range(500):
-            for j in range(500):
-                p.drawPoint(i, j)
-        self.r = (self.r + 40) % 255
-        self.g = (self.g + 40) % 255
-        self.b = (self.b + 40) % 255
-        p.end()
-        self.emit(SIGNAL('LoadedImage()'), )
+        print("hIihihi")
+        return
+
+class compute(QThread):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        for i in range(1000000):
+            print("HIHIHIHIHI\n")
         return
 
 
-class Application(QMainWindow):
+class setUI(object):
+    def setupUI(self, MainWindow):
+        MainWindow.setObjectName("Testing Window")
+        MainWindow.resize(1000, 650)
+        MainWindow.setMinimumSize(QSize(1000, 600))
+        MainWindow.setMaximumSize(QSize(1000, 600))
+        MainWindow.setTabShape(QTabWidget.Rounded)
+        self.centralWidget = QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.ScanB = QPushButton(self.centralWidget)
+        self.ScanB.setGeometry(QRect(630, 50, 131, 31))
+        self.ScanB.setCheckable(True)
+        self.ScanB.setObjectName("ScanB")
+
+
+class GUI(QMainWindow):
+    defw = 1000
+    defh = 600
+
     def __init__(self):
         super().__init__()
         self.scanA = QImage('Yellow_BG.JPG')
         self.scanPixmap = QPixmap()
         self.scanLabel = QLabel('Scan Area', self)
-
-        self.computer = compute()
-        self.computer.start()
-
+        self.scanLabel.setFixedSize(500, 500)
         self.initUI()
-        self.initFuncs()
+
+    r = 0
+    g = 0
+    b = 0
 
     def showImage(self):
         self.scanPixmap.convertFromImage(self.scanA)
         self.scanLabel.setPixmap(self.scanPixmap)
         return
 
+    def showGivenImage(self, image):
+        self.scanPixmap.convertFromImage(image)
+        self.scanLabel.setPixmap(self.scanPixmap)
+        return
+
     def drawImage(self):
         p = QPainter()
         p.begin(self.scanA)
-        # p.fillRect(0, 0, 500, 500, QColor(self.r, self.g, self.b, 250))
         p.setPen(QColor(self.r, self.g, self.b, 250))
         for i in range(500):
             for j in range(500):
@@ -69,34 +81,53 @@ class Application(QMainWindow):
 
     def updateImage(self):
         for i in range(10):
-
+            self.drawImage()
             self.showImage()
 
-    def initFuncs(self):
+    def initUI(self):
         self.drawImage()
         self.showImage()
         self.scanLabel.move(50, 50)
         self.scanLabel.show()
 
-        self.connect(self.computer, SIGNAL(""))
+        self.drawImage()
+        self.showImage()
 
-    def initUI(self):
         quitb = QPushButton('Quit', self)
         quitb.clicked.connect(QApplication.instance().quit)
         quitb.resize(quitb.sizeHint())
-        quitb.move(defw - 150, 60)
+        quitb.move(self.defw - 150, 60)
 
         scanb = QPushButton('Start Scan', self)
         scanb.clicked.connect(self.updateImage)
 
-        scanb.resize(scanb.sizeHint())
-        scanb.move(defw - 150, 120)
+        # all you have to do now is connect the mainwindow "start work" sig to the master slot
+        # which then starts a compute thread. the compute thread will periodically emit
+        # a signal connected to another master slot that emits a signal to the window slot
+        # that displays the image........hope this works
 
-        self.setFixedWidth(defw)
-        self.setFixedHeight(defh)
+        scanb.resize(scanb.sizeHint())
+        scanb.move(self.defw - 150, 120)
+
+        self.setFixedWidth(self.defw)
+        self.setFixedHeight(self.defh)
         self.setWindowTitle('SEM Visualization Demo')
         self.show()
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Application()
-    sys.exit(app.exec_())
+
+
+class master():
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.window = GUI()
+        self.setu = setUI()
+        self.setu.setupUI(self.window)
+        self.displayTh = compute()
+        self.displayTh.start()
+        self.dataTh = data()
+
+        self.window.show()
+        sys.exit(self.app.exec_())
+
+
+if __name__ == "__main__":
+    c = master()
--- Testing2.py
+++ Testing2.py
@@ -10,59 +10,43 @@ class Worker(QThread):
     def __init__(self):
         super()
         QThread.__init__(self)
-        # self.sendMessage = pyqtSignal(str)
 
     def run(self):
         self.sendMessage.emit("HIIIII")
         return
 
 
-class Controller():
+class coolApp(QApplication):
+    hireWorker = pyqtSignal()
+
     def __init__(self):
-        app = QCoreApplication([])
+        super().__init__([])
+        self.hireWorker.emit()
+
+    def saystuff(self, message):
+        print(message + "!!!")
+
+
+class Controller:
+    def __init__(self):
+        app = coolApp()
+        app.hireWorker.connect(self.saystuff)
         print("Starting...")
         worker = Worker()
         worker.finished.connect(app.exit)
-        worker.sendMessage.connect(self.saystuff)
+        worker.sendMessage.connect(app.saystuff)
+
         worker.start()
         sys.exit(app.exec_())
 
     def saystuff(self, message):
         print(message)
 
+    def startWorker(self):
+        worker2 = Worker()
+        worker2.sendMessage.connect(self.saystuff)
+        worker2.start()
+
 
 if __name__ == "__main__":
     c = Controller()
-
-# import sys
-# import time
-#
-# from PyQt5.QtCore import *
-#
-#
-# # Subclassing QThread
-# # http://qt-project.org/doc/latest/qthread.html
-# class AThread(QThread):
-#
-#     def run(self):
-#         count = 0
-#         while count < 5:
-#             time.sleep(1)
-#             print("A Increasing")
-#             count += 1
-#
-#
-# class Controller:
-#     def __init__(self):
-#         self.using_q_thread()
-#
-#     def using_q_thread(self):
-#         app = QCoreApplication([])
-#         thread = AThread()
-#         thread.finished.connect(app.exit)
-#         thread.start()
-#         sys.exit(app.exec_())
-#
-#
-# if __name__ == "__main__":
-#     c = Controller()
