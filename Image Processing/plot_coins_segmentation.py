5f60e1647ce7af6ffb787daf1a08f06d6e5b3dfd
BYTurnips <brionqiye@gmail.com>
2018-12-23 21:12:04 -0800

    sobel -> elevation -> watershed -> ID starter code looks pretty effective

--- Image Processing/plot_coins_segmentation.py
+++ Image Processing/plot_coins_segmentation.py
@@ -1,21 +1,9 @@
-"""
-==================================================
-Comparing edge-based and region-based segmentation
-==================================================
-
-In this example, we will see how to segment objects from a background. We use
-the ``coins`` image from ``skimage.data``, which shows several coins outlined
-against a darker background.
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-from skimage import data
 from skimage.exposure import histogram
 from skimage.io import imread
 
-# coins = data.coins()
 coins = imread('Init_250.bmp', as_gray=True)
 hist, hist_centers = histogram(coins)
 
@@ -25,84 +13,15 @@ axes[0].axis('off')
 axes[1].plot(hist_centers, hist, lw=2)
 axes[1].set_title('histogram of gray values')
 
-######################################################################
-#
-# Thresholding
-# ============
-#
-# A simple way to segment the coins is to choose a threshold based on the
-# histogram of gray values. Unfortunately, thresholding this image gives a
-# binary image that either misses significant parts of the coins or merges
-# parts of the background with the coins:
-
-# fig, axes = plt.subplots(1, 2, figsize=(8, 3), sharey=True)
-#
-# axes[0].imshow(coins > 100, cmap=plt.cm.gray, interpolation='nearest')
-# axes[0].set_title('coins > 100')
-#
-# axes[1].imshow(coins > 150, cmap=plt.cm.gray, interpolation='nearest')
-# axes[1].set_title('coins > 150')
-#
-# for a in axes:
-#     a.axis('off')
-#
-# plt.tight_layout()
-
-######################################################################
-# Edge-based segmentation
-# =======================
-#
-# Next, we try to delineate the contours of the coins using edge-based
-# segmentation. To do this, we first get the edges of features using the
-# Canny edge-detector.
-
-# from skimage.feature import canny
-#
-# edges = canny(coins)
-#
-# fig, ax = plt.subplots(figsize=(4, 3))
-# ax.imshow(edges, cmap=plt.cm.gray, interpolation='nearest')
-# ax.set_title('Canny detector')
-# ax.axis('off')
-
-######################################################################
-# These contours are then filled using mathematical morphology.
-
 from scipy import ndimage as ndi
-#
-# fill_coins = ndi.binary_fill_holes(edges)
-#
-# fig, ax = plt.subplots(figsize=(4, 3))
-# ax.imshow(fill_coins, cmap=plt.cm.gray, interpolation='nearest')
-# ax.set_title('filling the holes')
-# ax.axis('off')
-
-
-######################################################################
-# Small spurious objects are easily removed by setting a minimum size for
-# valid objects.
-
 from skimage import morphology
-#
-# coins_cleaned = morphology.remove_small_objects(fill_coins, 21)
-#
-# fig, ax = plt.subplots(figsize=(4, 3))
-# ax.imshow(coins_cleaned, cmap=plt.cm.gray, interpolation='nearest')
-# ax.set_title('removing small objects')
-# ax.axis('off')
+from skimage.filters import sobel
 
-######################################################################
-# However, this method is not very robust, since contours that are not
-# perfectly closed are not filled correctly, as is the case for one unfilled
-# coin above.
-#
 # Region-based segmentation
 # =========================
 #
 # We therefore try a region-based method using the watershed transform.
 # First, we find an elevation map using the Sobel gradient of the image.
-
-from skimage.filters import sobel
 
 elevation_map = sobel(coins)
 
