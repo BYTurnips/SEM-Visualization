f9d5a4598b764609228d0169fbcf32b8d9c140b3
BYTurnips <brionqiye@gmail.com>
2018-12-27 14:29:42 -0800

    could not do simple interp2d because that function is a big lie
    so did griddata into interp2d instead.

--- Image Processing/Image Process White.py
+++ Image Processing/Image Process White.py
@@ -11,24 +11,44 @@
 # linear interpolation is used to transform the image for all future samples.
 
 import numpy as np
-from scipy import spatial as spa
+import scipy.interpolate as inter
 from scipy import ndimage as ndi
 import skimage as ski
 from skimage import morphology
 import matplotlib.pyplot as plt
+from PyQt5 import QtGui as qgui
 
 squarewidth = 20
-squaredist = 10
+squaredist = 5
 img_size = 250
 
 
+# rotated, streched, offset grid parameters (close to ideal): (35, 26, 10, 15, 15, 0)
+
 class RegionFinder:
     def __init__(self):
-        fig, self.axes = plt.subplots(1, 2, figsize=(8, 3), sharey=True)
-        fig, self.ergraph = plt.subplots(1, 1, figsize=(8, 3), sharey=True)
+        fig, self.axes = plt.subplots(1, 3, figsize=(8, 3), sharey=True)
+        # fig, self.ergraph = plt.subplots(1, 1, figsize=(8, 3), sharey=True)
         self.init = ski.io.imread('Init_250.bmp', as_gray=True)
+
         self.warpcenters = self.findWarpedCenters()
-        self.gridGradDesc()
+        # self.warpcenters = self.getIdealGrid(30, 10, 27, 10, 15)
+        xs = [x[1] for x in self.warpcenters]
+        ys = [x[0] for x in self.warpcenters]
+        self.axes[1].scatter(xs, ys)
+        self.axes[2].scatter(xs, ys)
+
+        # self.ideal = self.findGrid()
+        self.ideal = self.getIdealGrid(35, 26, 10, 15, 15, 0)
+        # print(self.testGridLoss(35, 26, 10, 15, 15, 0))
+        xs = [x[1] for x in self.ideal]
+        ys = [x[0] for x in self.ideal]
+        self.axes[0].scatter(xs, ys, s=10)
+
+        self.lutx, self.luty = self.generateMapping(self.warpcenters, self.ideal)
+        print(self.init[2][100])
+        plt.tight_layout()
+        plt.show()
 
     def findWarpedCenters(self):
         init = self.init
@@ -52,10 +72,6 @@ class RegionFinder:
         self.axes[0].imshow(init, cmap=plt.cm.gray, interpolation='nearest')
         self.axes[0].contour(segmentation, [0.5], linewidths=1.2, colors='y')
         self.axes[1].imshow(image_label_overlay, interpolation='nearest')
-        # self.axes[2].imshow(init, cmap=plt.cm.gray, interpolation='nearest')
-
-        # for a in self.axes:
-        #     a.axis('off')
 
         for k in properties:
             if 0.5 < k.area / avg_area < 1.5:
@@ -64,103 +80,133 @@ class RegionFinder:
                 for q in range(len(k.coords)):
                     newcoords[q][1] = k.coords[q][0]
                     newcoords[q][0] = k.coords[q][1]
-                # axes[2].add_patch(pat.Polygon(self.minimum_bounding_rectangle(newcoords)))
 
         centers = [(round(x), round(y)) for x, y in centers]
-
-        xs = [x[1] for x in centers]
-        ys = [x[0] for x in centers]
-        self.axes[1].scatter(xs, ys)
 
         return centers
 
     def findGrid(self):
-        width = squarewidth + squaredist
-        grid_size = 12
+        w = squarewidth + squaredist
+        h = squarewidth + squaredist
+        size = 12
         curmin = 1000000000
-        ares = 90
+        ares = 18
         tres = 5
-        mina = 0
-        minx = 0
-        miny = 0
+        mina, minx, miny = 0, 0, 0
         for a in range(ares):
-            sumofPE = self.testGridLoss(width, grid_size, 90 * a / ares, 0, 0)
-            if sumofPE < curmin:
-                curmin = sumofPE
-                mina = a
-                # print(curmin)
+            print(a)
+            for x in range(tres):
+                for y in range(tres):
+                    A = 90 * a / ares
+                    X = w * x / tres
+                    Y = w * y / tres
+                    sumofPE = self.testGridLoss(w, h, size, A, X, Y)
+                    if sumofPE < curmin:
+                        curmin = sumofPE
+                        mina = a
+                        minx = x
+                        miny = y
+                        print(curmin, minx, miny)
+        finalideal = self.getIdealGrid(w, h, size, mina, w * minx / tres, w * miny / tres)
 
-        for x in range(tres):
-            for y in range(tres):
-                sumofPE = self.testGridLoss(width, grid_size, mina, width * x / tres, width * y / tres)
-                if sumofPE < curmin:
-                    curmin = sumofPE
-                    minx = x
-                    miny = y
-                    print(curmin, minx, miny)
-        minx = 0
-        miny = 0
-        finalideal = self.getIdealGrid(width, grid_size, mina, width * minx / tres, width * miny / tres)
-        xs = [x[1] for x in finalideal]
-        ys = [x[0] for x in finalideal]
-        self.axes[0].scatter(xs, ys, s=10)
+        return finalideal
 
-        plt.tight_layout()
-        plt.show()
-
-    def gridGradDesc(self):
-        width = squarewidth + squaredist
-        grid_size = 10
-        mina = 0
-        A = 0
-        X = 0
-        Y = 0
-        learn = 0.5
-
-        for cycle in range(40):
-            # Finding the initial loss
-            init_loss = self.testGridLoss(width, grid_size, A, X, Y)
-            self.ergraph.scatter(cycle, init_loss, s=10)
-            # Finding the partial derivatives aka the gradient
-            dA = (self.testGridLoss(width, grid_size, A + 0.0001, X, Y) - init_loss) / 0.0001
-            dX = (self.testGridLoss(width, grid_size, A, X + 0.0001, Y) - init_loss) / 0.0001
-            dY = (self.testGridLoss(width, grid_size, A, X, Y + 0.0001) - init_loss) / 0.0001
-
-            print(round(A, 3), round(X, 3), round(Y, 3), round(init_loss, 3), "D/DL",
-                  round(dA, 3), round(dX, 3), round(dY, 3))
-
-            A -= learn * dA
-            X -= learn * dX
-            Y -= learn * dY
-
-        finalideal = self.getIdealGrid(width, grid_size, A, X, Y)
-        xs = [x[1] for x in finalideal]
-        ys = [x[0] for x in finalideal]
-        self.axes[0].scatter(xs, ys, s=10)
-
-        plt.tight_layout()
-        plt.show()
-
-    def testGridLoss(self, sqsize, numsquare, angle, tranX, tranY):
-        t = sqsize / 3
-        idealgrid = self.getIdealGrid(sqsize, numsquare, angle, tranX, tranY)
+    def testGridLoss(self, sqh, sqv, numsquare, angle, tranX, tranY):
+        t = (squarewidth + squaredist) / 3
+        idealgrid = self.getIdealGrid(sqh, sqv, numsquare, angle, tranX, tranY)
         sumofPE = 0
         for i in self.warpcenters:
             for j in idealgrid:
                 d = np.sqrt(pow(i[0] - j[0], 2) + pow(i[1] - j[1], 2))
-                sumofPE += 10 * np.exp(-d / t)
+                sumofPE += -10 * np.exp(-d / t)
                 # sumofPE += pow(d, 2)/5000
         return sumofPE
 
-    def getIdealGrid(self, sqsize, numsquare, angle, tranX, tranY):
+    def generateMapping(self, warped, ideal):
+        # sorted(warped, key=self.distFromMiddle)
+        # middlept = warped[0]
+
+        # might have to change the way the points are being mapped - currently
+        # just a simple closest-neighbor method
+        x, y, dx, dy = [], [], [], []
+        for warp in warped:
+            close = [1000, 1000]
+            curmin = 100000000
+            for ipt in ideal:
+                if self.distBWPts(ipt, warp) < curmin:
+                    curmin = self.distBWPts(ipt, warp)
+                    close = ipt
+            x.append(warp[0])
+            y.append(warp[1])
+            dx.append(close[0] - warp[0])
+            dy.append(close[1] - warp[1])
+        x = np.asarray(x)
+        y = np.asarray(y)
+        dx = np.asarray(dx)
+        dy = np.asarray(dy)
+        coors = np.column_stack((x, y))
+        rows = np.arange(0, 251, 25)
+        cols = np.arange(0, 251, 25)
+        grid_x, grid_y = np.mgrid[0:250:11j, 0:250:11j]
+        tlutx = inter.griddata(coors, dx, (grid_x, grid_y))
+        tluty = inter.griddata(coors, dy, (grid_x, grid_y))
+
+        # print(rows, cols, tlutx)
+        lutx = inter.interp2d(rows, cols, tlutx)
+        luty = inter.interp2d(rows, cols, tluty)
+        return lutx, luty
+
+    def convertPicture(self):
+        processed = qgui.QImage('grid.png')
+
+    def gridGradDesc(self):
+        w = squarewidth + squaredist
+        size = 10
+        A = 0
+        X = 0
+        Y = 0
+        alearn = 0.01
+        xylearn = 0.01
+
+        for cycle in range(100):
+            # Finding the initial loss
+            init_loss = self.testGridLoss(w, size, A, X, Y)
+            self.ergraph.scatter(cycle, init_loss, s=10)
+            # Finding the partial derivatives aka the gradient
+            dA = (self.testGridLoss(w, size, A + 0.000001, X, Y) - init_loss) / 0.000001
+            dX = (self.testGridLoss(w, size, A, X + 0.000001, Y) - init_loss) / 0.000001
+            dY = (self.testGridLoss(w, size, A, X, Y + 0.000001) - init_loss) / 0.000001
+
+            print(round(A, 3), round(X, 3), round(Y, 3), round(init_loss, 3), "D/DL",
+                  round(dA, 3), round(dX, 3), round(dY, 3))
+
+            A -= alearn * dA
+            X -= xylearn * dX
+            Y -= xylearn * dY
+
+        finalideal = self.getIdealGrid(w, size, A, X, Y)
+        xs = [x[1] for x in finalideal]
+        ys = [x[0] for x in finalideal]
+        self.axes[0].scatter(xs, ys, s=10)
+        self.axes[2].scatter(xs, ys, s=10)
+
+        plt.tight_layout()
+        plt.show()
+
+        return finalideal
+
+    def distBWPts(self, c1, c2):
+        return np.sqrt(pow(c2[0] - c1[0], 2) + pow(c2[1] - c1[1], 2))
+
+    def getIdealGrid(self, sqh, sqv, numsquare, angle, tranX, tranY):
         cors = []
         for i in range(numsquare):
             for j in range(numsquare):
                 ang = np.deg2rad(angle)
-                cors.append([(i - float(numsquare - 1) / 2) * sqsize * np.cos(ang) + tranX -
-                             (j - float(numsquare - 1) / 2) * sqsize * np.sin(ang) + img_size / 2,
-                             (i - float(numsquare - 1) / 2) * sqsize * np.sin(ang) + tranY +
-                             (j - float(numsquare - 1) / 2) * sqsize * np.cos(ang) + img_size / 2])
+                cors.append([(i - float(numsquare - 1) / 2) * sqh * np.cos(ang) + tranX -
+                             (j - float(numsquare - 1) / 2) * sqv * np.sin(ang) + img_size / 2,
+                             (i - float(numsquare - 1) / 2) * sqh * np.sin(ang) + tranY +
+                             (j - float(numsquare - 1) / 2) * sqv * np.cos(ang) + img_size / 2])
         return cors
 
 
--- Testing.py
+++ Testing.py
@@ -1,16 +1,39 @@
 import numpy as np
+import scipy.interpolate as inter
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from time import perf_counter
 from collections import deque
 from matplotlib import pyplot as plt
 
-if __name__ == '__main__':
-    _, axes = plt.subplots(1, 4, figsize=(8, 3), sharey=True)
-    xs = [30]
-    ys = [100]
-    x = 30
-    y = 30
 
-    axes[0].scatter(x, y)
-    plt.show()
+# # x = np.arange(0, 5, 1)
+# # y = np.arange(0, 5, 1)
+# x = np.asarray([0, 1, 2, 3, 4])
+# y = np.asarray([0, 1, 2, 3, 4])
+# xx, yy = np.meshgrid(x, y)
+# # z = np.sin(xx*2+yy*2)
+# z = np.asarray([[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]])
+# f = inter.interp2d(x, y, z)
+
+def func(x, y):
+    return x + y
+
+
+grid_x, grid_y = np.mgrid[0:25:6j, 0:25:6j]
+points = np.random.rand(10, 2) * 25
+points = points.astype(int)
+values = func(points[:, 0], points[:, 1])
+
+# print(points)
+# print(values)
+
+grid = inter.griddata(points, values, (grid_x, grid_y))
+
+row = np.mgrid[0:25:6j]
+col = np.mgrid[0:25:6j]
+
+print(row)
+print(col)
+print(grid)
+lut = inter.interp2d(row, col, grid)
